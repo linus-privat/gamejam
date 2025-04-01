5525bf59360d65d866460109acb24f025454daf2
Linusrmy <linus.remy@gmx.de>
2025-04-01 21:17:56 +0200

    added AnimationPalyer

--- gamejamgame/scenes/player.gd
+++ gamejamgame/scenes/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-const max_speed = 400
-const acceleration = 1500
-const friction = 600
+const max_speed = 600
+const acceleration = 2300
+const friction = 1800
 
 var input = Vector2.ZERO
 
@@ -10,14 +10,19 @@ func _physics_process(delta):
 	player_movement(delta)
 	
 func player_movement(delta):
-		input = Input.get_vector("walk left", "walk right", "walk up", "walk down")
+		input = Input.get_vector("walk left", "walk right", "walk up", "walk down")		
 		if input == Vector2.ZERO:
 			if velocity.length() > (friction * delta):
 				velocity -= velocity.normalized() * (friction * delta)
 			else:
 				velocity = Vector2.ZERO
 		else:
-			velocity += (input * acceleration * delta)
+			velocity += (input * acceleration)
 			velocity = velocity.limit_length(max_speed)
 		move_and_slide()
+
+
+	
+
+
 				
