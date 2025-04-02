8eb46a61e9a31b4e5d61cc3e92f4bd1b654624bc
Linusrmy <linus.remy@gmx.de>
2025-04-02 14:53:44 +0200

    shoot marker left hinzugefügt und code aktualisiert

--- gamejamgame/scenes/linus_test/tileset_test.gd
+++ gamejamgame/scenes/linus_test/tileset_test.gd
@@ -6,9 +6,12 @@ signal addtime(seconds)
 
 var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
 
-func _on_player_shoot(pos, direction: Vector2) -> void:
+func _on_player_shoot(pos_r,pos_l, direction: Vector2) -> void:
 	var bullet = bullet_scene.instantiate() as RigidBody2D
-	bullet.position = pos
+	if direction.x <0:
+		bullet.position = pos_l
+	else:
+		bullet.position = pos_r
 	bullet.linear_velocity = direction * bullet.speed
 	print(direction)
 	bullet.rotation = direction.angle()
