52cf5f261d09ee4f1108c3a874cb914969810ce0
Linusrmy <linus.remy@gmx.de>
2025-04-02 15:37:57 +0200

    fixed level1 dependencies

---
+++ gamejamgame/scenes/linus_test/level_1.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+class_name Level1
+
+signal addtime(seconds)
+
+var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
+
+func _on_player_shoot(pos_r,pos_l, direction: Vector2) -> void:
+	var bullet = bullet_scene.instantiate() as RigidBody2D
+	if direction.x <0:
+		bullet.position = pos_l
+	else:
+		bullet.position = pos_r
+	bullet.linear_velocity = direction * bullet.speed
+	print(direction)
+	bullet.rotation = direction.angle()
+	
+	$Projectiles.add_child(bullet)
+	
+	addtime.emit(10)
